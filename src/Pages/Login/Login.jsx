f913b7e93471a479cdd03b3920ba2e0d98c7ea3a
Gabs Ghidini <gabsghidini@gmail.com>
2022-10-06 16:26:41 -0300

    Feature: register page fully functional

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,21 +1,52 @@
 import Logo from "../../assets/hub.png";
 import * as S from "./styles";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+	const navigate = useNavigate();
+	const [email, setEmail] = useState(null);
+	const [password, setPassword] = useState(null);
+	const data = {
+		email: email,
+		password: password,
+	};
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+
+		console.log(data);
+	};
+
+	const handleRedirect = () => {
+		navigate("/register");
+	};
 	return (
 		<S.Wrapper>
 			<img src={Logo} alt="Kenzie Hub logo" />
 			<S.Form>
 				<S.Title>Login</S.Title>
 				<label>Email</label>
-				<S.Input type="text" placeholder="eevee@gmail.com" />
+				<S.Input
+					type="text"
+					placeholder="eevee@gmail.com"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
 				<label>Senha</label>
-				<S.Input type="text" placeholder="S3nh4 Sup3r S3cr3t4" />
-				<S.LoginButton>Entrar</S.LoginButton>
+				<S.Input
+					type="password"
+					placeholder="S3nh4 Sup3r S3cr3t4"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<S.LoginButton type="submit" onClick={handleLogin}>
+					Entrar
+				</S.LoginButton>
 				<S.HeadlineBoldSmall className="bold">
 					Ainda não possui uma conta?
 				</S.HeadlineBoldSmall>
-				<S.RegisterButton>Cadastre-se</S.RegisterButton>
+				<S.RegisterButton onClick={handleRedirect}>
+					Cadastre-se
+				</S.RegisterButton>
 			</S.Form>
 		</S.Wrapper>
 	);
