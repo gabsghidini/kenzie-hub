b3b44a292e4fd1b83ddda8a029427484acecd44a
gabsghidini <gabsghidini@gmail.com>
2022-10-13 14:42:06 -0300

    Feature: context usage to avoid prop drilling

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
 import { BrowserRouter } from "react-router-dom";
+import UserProvider from "./Contexts/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<UserProvider>
+				<App />
+			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
