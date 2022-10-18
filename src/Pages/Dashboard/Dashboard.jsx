91021332d4f5be59c5d1f7e518cfbf31e1caf563
gabsghidini <gabsghidini@gmail.com>
2022-10-18 16:07:12 -0300

    Feat: user now autologins

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,13 +2,16 @@ import Navbar from "../../Components/Navbar";
 import Header from "../../Components/Header";
 import Techs from "../../Components/Techs";
 import { Container } from "./styles";
+import ProtectedRoutes from "../../Components/ProtectedRoutes";
 
 const Dashboard = () => {
 	return (
 		<Container>
-			<Navbar />
-			<Header />
-			<Techs />
+			<ProtectedRoutes>
+				<Navbar />
+				<Header />
+				<Techs />
+			</ProtectedRoutes>
 		</Container>
 	);
 };
--- src/Routes/HubRoutes.jsx
+++ src/Routes/HubRoutes.jsx
@@ -3,13 +3,16 @@ import Login from "../Pages/Login";
 import Register from "../Pages/Register";
 import Dashboard from "../Pages/Dashboard";
 import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoutes from "../Components/ProtectedRoutes";
 
 const HubRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Login />} />
 			<Route path="/register" element={<Register />} />
-			<Route path="/dashboard" element={<Dashboard />} />
+			<Route element={<ProtectedRoutes />}>
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Route>
 			<Route path="*" element={<Navigate to="/" />} />
 		</Routes>
 	);
