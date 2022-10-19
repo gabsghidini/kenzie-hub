a5a77e4f55cf2db5c09c55e0534d707a13855638
gabsghidini <gabsghidini@gmail.com>
2022-10-19 16:39:50 -0300

    Fix: Autologin had a wrong import, now fixed

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,16 +2,13 @@ import Navbar from "../../Components/Navbar";
 import Header from "../../Components/Header";
 import Techs from "../../Components/Techs";
 import { Container } from "./styles";
-import ProtectedRoutes from "../../Components/ProtectedRoutes";
 
 const Dashboard = () => {
 	return (
 		<Container>
-			<ProtectedRoutes>
-				<Navbar />
-				<Header />
-				<Techs />
-			</ProtectedRoutes>
+			<Navbar />
+			<Header />
+			<Techs />
 		</Container>
 	);
 };
